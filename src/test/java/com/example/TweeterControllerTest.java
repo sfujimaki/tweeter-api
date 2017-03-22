0973a4eb1c8f8c57afe9b8f0ba9edfd80c73c275
making <makingx@gmail.com>
2017-03-22 12:17:53 +0900

    Add RestDocs

--- src/test/java/com/example/TweeterControllerTest.java
+++ src/test/java/com/example/TweeterControllerTest.java
@@ -2,11 +2,17 @@ package com.example;
 
 import static org.hamcrest.Matchers.*;
 import static org.mockito.BDDMockito.given;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
+import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.restassured3.operation.preprocess.RestAssuredPreprocessors.modifyUris;
 
 import java.util.HashMap;
 import java.util.Map;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Matchers;
@@ -15,17 +21,24 @@ import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpHeaders;
+import org.springframework.restdocs.JUnitRestDocumentation;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.util.Base64Utils;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class TweeterControllerTest {
+	@Rule
+	public final JUnitRestDocumentation restDocumentation = new JUnitRestDocumentation(
+			"target/generated-snippets");
+	RequestSpecification documentationSpec;
 	@Autowired
 	ObjectMapper objectMapper;
 	@MockBean
@@ -36,17 +49,32 @@ public class TweeterControllerTest {
 	@Before
 	public void setUp() throws Exception {
 		RestAssured.port = port;
+		this.documentationSpec = new RequestSpecBuilder()
+				.addFilter(documentationConfiguration(this.restDocumentation)).build();
 	}
 
 	@Test
 	public void getTimelines() throws Exception {
 		given(tweeterMapper.findAll()).willReturn(Fixtures.tweetsAll());
 
-		RestAssured.given().header(HttpHeaders.AUTHORIZATION,
+		RestAssured.given(this.documentationSpec).header(HttpHeaders.AUTHORIZATION,
 				"Basic " + Base64Utils.encodeToString(("user:password".getBytes())))
-				.accept("application/json").log().all().when().get("/v1/timelines").then()
-				.log().all().assertThat().statusCode(is(200))
-				.contentType(ContentType.JSON).content("$", hasSize(4))
+				.accept("application/json")
+				.filter(document("api/get-timelines",
+						preprocessRequest(modifyUris().scheme("https")
+								.host("api.example.com").removePort()),
+						responseFields(
+								fieldWithPath("[].tweetId")
+										.description("The ID of this tweet"),
+								fieldWithPath("[].text")
+										.description("The text of this tweet"),
+								fieldWithPath("[].username")
+										.description("Owner's username of this tweet"),
+								fieldWithPath("[].createdAt")
+										.description("The date of this tweet"))))
+				.log().all().when().get("/v1/timelines").then().log().all().assertThat()
+				.statusCode(is(200)).contentType(ContentType.JSON)
+				.content("$", hasSize(4))
 				.body("[0].tweetId", is("00000000-0000-0000-0000-000000000000"))
 				.body("[0].text", is("tweet1")).body("[0].username", is("user"))
 				.body("[0].createdAt", notNullValue())
@@ -66,11 +94,23 @@ public class TweeterControllerTest {
 		given(tweeterMapper.findByUsername("user"))
 				.willReturn(Fixtures.tweetsByUsername("user"));
 
-		RestAssured.given().header(HttpHeaders.AUTHORIZATION,
+		RestAssured.given(this.documentationSpec).header(HttpHeaders.AUTHORIZATION,
 				"Basic " + Base64Utils.encodeToString(("user:password".getBytes())))
-				.accept("application/json").log().all().when().get("/v1/tweets").then()
-				.log().all().assertThat().statusCode(is(200))
-				.contentType(ContentType.JSON).body("$", hasSize(3))
+				.accept("application/json")
+				.filter(document("api/get-tweets",
+						preprocessRequest(modifyUris().scheme("https")
+								.host("api.example.com").removePort()),
+						responseFields(
+								fieldWithPath("[].tweetId")
+										.description("The ID of this tweet"),
+								fieldWithPath("[].text")
+										.description("The text of this tweet"),
+								fieldWithPath("[].username")
+										.description("Owner's username of this tweet"),
+								fieldWithPath("[].createdAt")
+										.description("The date of this tweet"))))
+				.log().all().when().get("/v1/tweets").then().log().all().assertThat()
+				.statusCode(is(200)).contentType(ContentType.JSON).body("$", hasSize(3))
 				.body("[0].tweetId", is("00000000-0000-0000-0000-000000000000"))
 				.body("[0].text", is("tweet1")).body("[0].username", is("user"))
 				.body("[0].createdAt", notNullValue())
@@ -89,12 +129,26 @@ public class TweeterControllerTest {
 		Map<String, Object> body = new HashMap<>();
 		body.put("text", "tweet1");
 
-		RestAssured.given().header(HttpHeaders.AUTHORIZATION,
+		RestAssured.given(this.documentationSpec).header(HttpHeaders.AUTHORIZATION,
 				"Basic " + Base64Utils.encodeToString(("user:password".getBytes())))
 				.contentType(ContentType.JSON).body(objectMapper.writeValueAsString(body))
-				.accept("application/json").log().all().when().post("/v1/tweets").then()
-				.log().all().assertThat().statusCode(is(201))
-				.contentType(ContentType.JSON)
+				.accept("application/json")
+				.filter(document("api/post-tweets",
+						preprocessRequest(modifyUris()
+								.scheme("https").host("api.example.com").removePort()),
+						requestFields(fieldWithPath("text")
+								.description("The text of this tweet")),
+						responseFields(
+								fieldWithPath("tweetId")
+										.description("The ID of this tweet"),
+								fieldWithPath("text")
+										.description("The text of this tweet"),
+								fieldWithPath("username")
+										.description("Owner's username of this tweet"),
+								fieldWithPath("createdAt")
+										.description("The date of this tweet"))))
+				.log().all().when().post("/v1/tweets").then().log().all().assertThat()
+				.statusCode(is(201)).contentType(ContentType.JSON)
 				.body("tweetId", is("00000000-0000-0000-0000-000000000000"))
 				.body("text", is("tweet1")).body("username", is("user"))
 				.body("createdAt", notNullValue());
