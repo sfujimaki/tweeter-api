355fa3e9944b901a52d91bdc54dce3d30be766b2
sfujimak <sfujimak@yahoo-corp.jp>
2017-04-19 11:58:01 +0900

    fix indent

--- src/test/resources/contracts/gettweets/shouldReturnUserTweetByPost.groovy
+++ src/test/resources/contracts/gettweets/shouldReturnUserTweetByPost.groovy
@@ -14,12 +14,10 @@ Contract.make {
     response {
         status 200
         body([
-                [
-                        tweetId  : '00000000-0000-0000-0000-000000000000',
-                        text     : 'tweet1',
-                        username : 'user',
-                        createdAt: $(client('2017-04-12T01:09:05Z'), server(regex('[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}Z')))
-                ]
+            tweetId  : '00000000-0000-0000-0000-000000000000',
+            text     : 'tweet1',
+            username : 'user',
+            createdAt: $(client('2017-04-12T01:09:05Z'), server(regex('[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}Z')))
         ])
         headers {
             contentType('application/json')
